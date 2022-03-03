66f3f85e320fd7476d82684e3144bb95fd60b833
smjeffery22 <parkjon8@gmail.com>
2022-03-03 17:03:20 -0500

    Add a post route for adding a new campsite

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
+const campground = require('./model/campground');
 const PORT = 3000;
 
 const Campground = require('./model/campground');
@@ -18,14 +19,27 @@ db.once('open', () => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
 	res.render('home');
+});
+
+app.post('/campgrounds', async (req, res) => {
+	const campground = new Campground(req.body.campground);
+  await campground.save();
+
+	res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get('/campgrounds', async (req, res) => {
 	const campgrounds = await Campground.find({});
 
 	res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+	res.render('campgrounds/new');
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
