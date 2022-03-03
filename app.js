9535d944991c767a505887c870f90ebe1662a44f
smjeffery22 <parkjon8@gmail.com>
2022-03-03 16:42:33 -0500

    Set up a get route to display all campsites

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ app.get('/', (req, res) => {
 	res.render('home');
 });
 
-app.get('/makecampground', async (req, res) => {
-	const camp = new Campground({ title: 'My Backyard', description: 'home camping' });
-  await camp.save();
+app.get('/campgrounds', async (req, res) => {
+	const campgrounds = await Campground.find({});
 
-  res.send(camp);
+	res.render('campgrounds/index', { campgrounds });
 });
 
 app.listen(PORT, () => {
