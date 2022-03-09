246a4c2e28c3d9eae02b0881b6516af282c2833c
smjeffery22 <parkjon8@gmail.com>
2022-03-09 15:08:21 -0500

    Refactor code to extract form validation function

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/campgrounds', campgroundsRoutes);
 app.use('/campgrounds/:id/reviews', reviewsRoutes);
