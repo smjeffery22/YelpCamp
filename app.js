a6bcb20f6dace4a4c73e97832a837f3632290fa2
smjeffery22 <parkjon8@gmail.com>
2022-03-09 00:01:01 -0500

    Add delete route for deleting reviews

--- app.js
+++ app.js
@@ -114,13 +114,22 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
 app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
 	const campground = await Campground.findById(req.params.id);
 	const review = new Review(req.body.review);
-	console.log('---------', review)
+
 	campground.reviews.push(review);
 	await  review.save();
 	await campground.save();
 
 	res.redirect(`/campgrounds/${campground._id}`);
 }))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res) => {
+	const { id, reviewId } = req.params;
+	
+	await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
+	await Review.findByIdAndDelete(reviewId);
+
+	res.redirect(`/campgrounds/${id}`);
+}));
 
 app.all('*', (req, res, next) => {
 	next(new expressError('Page Not Found', 404));
