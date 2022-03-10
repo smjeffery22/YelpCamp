c51433edcfc51b29bd3929ab1dcc78af59fb1bce
smjeffery22 <parkjon8@gmail.com>
2022-03-09 21:00:02 -0500

    Add login route and page, and implement passport for register and login

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,35 @@
 const express = require('express');
-const router = express.Router();
+const catchAsync = require('../utils/catchAsync')
+const passport = require('passport')
 const User = require('../model/user');
 
-router.post('/register', async(req, res) => {
-  res.send(req.body);
-})
+const router = express.Router();
+
+router.post('/register', catchAsync(async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = new User({ username, email });
+    const registeredUser = await User.register(user, password);
+    
+    req.flash('success', 'Welcome to Yelp Camp!');
+    res.redirect('/campgrounds');
+  } catch(e) {
+    req.flash('error', e.message);
+    res.redirect('/register')
+  }
+}));
 
 router.get('/register', (req, res) => {
   res.render('users/register');
-})
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+  req.flash('success', 'Welcome back!');
+  res.redirect('/campgrounds');
+});
+
+router.get('/login', (req, res) => {
+  res.render('users/login');
+});
 
 module.exports = router;
